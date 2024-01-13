876e997483c2d0fc4ce43bffc10a38bb69284575
Prajwal_RV <workwithprajwalrv1@gmail.com>
2024-01-13 17:11:09 +0530

    Part - 2 assignment started

---
+++ KunalKushwaha/Assignments/RupeesToDollars.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RupeesToDollars {
+    public static void main(String[] args) {
+        // 6. Input currency in rupees and output in USD.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Deposit your INR to get the dollars worth of it : ");
+        double inr = in.nextInt();
+
+        double usd = inr / 85;
+
+        System.out.println("$ "+ usd);
+    
+    }
+}
