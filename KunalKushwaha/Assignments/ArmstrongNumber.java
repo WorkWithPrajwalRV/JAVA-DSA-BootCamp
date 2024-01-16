b13251ce60e51a2a597ef4e91ed5952ea0922887
Prajwal_RV <workwithprajwalrv1@gmail.com>
2024-01-16 11:02:02 +0530

    Assignment part-2 completed

--- KunalKushwaha/Assignments/ArmstrongNumber.java
+++ KunalKushwaha/Assignments/ArmstrongNumber.java
@@ -4,23 +4,39 @@ public class ArmstrongNumber {
     public static void main(String[] args) {
         // 9. To find Armstrong Number between two given number. 
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter number to check is it Armstrong Number or not : ");
-        int number = in.nextInt();
+        System.out.println("Enter the range to get armstrong number between that range : ");   
+        System.out.print("StartRange = ");
+        int StartRange = in.nextInt();
+        System.out.print("EndRange = ");
+        int EndRange = in.nextInt();
 
-        int power = String.valueOf(number).length(); // to count number of digits in a number
-        int sum = 0;
-        int temp = number;
-        System.out.println(power);
-        for (int i=0;i<power;i++) {
-            int lastDigit = temp % 10; // to extract last digit => 3, 5
-            sum += lastDigit * power; // 3 * 3 * 3 = 27, 5 * 5 * 5 = 125 => 27 + 125 = 152,
-            temp = temp/10;
-        }
-        System.out.println(sum);
-        if (sum == number) {
-            System.out.println("Armstrong Number");
-        } else {
-            System.out.println("Not a armstrong number");
+        
+
+        for (int number = StartRange; number <= EndRange; number++) {
+
+            int digits = 0;
+            int result = 0;
+            int originalNummber = number;
+            
+            // Conuting digits in a number
+            while (originalNummber != 0) {
+                originalNummber /= 10;
+                ++digits;
+            }
+
+            originalNummber = number;
+
+            // Performing Armstong operation on number 
+            while (originalNummber != 0) {
+                int remainder = originalNummber % 10;
+                result += Math.pow(remainder, digits);
+                originalNummber /= 10;
+            }
+
+            // Printing armstring number b/w the range
+            if (result == number) {
+                System.out.print(number + " ");
+            }
         }
     }
 }
