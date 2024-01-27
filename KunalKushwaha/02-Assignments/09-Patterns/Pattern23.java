19663284130e5bc1a14e12e12ade210c8cd82cf6
Prajwal_RV <workwithprajwalrv1@gmail.com>
2024-01-27 23:05:24 +0530

    patern updated

--- KunalKushwaha/02-Assignments/09-Patterns/Pattern23.java
+++ KunalKushwaha/02-Assignments/09-Patterns/Pattern23.java
@@ -6,7 +6,7 @@ public abstract class Pattern23 {
         int n = input.nextInt();
         int numberOfSpace = n/2 + 1;
         int numberOfStars = 2;
-        int numberOfSpaceAfterStar = 2;
+        int numberOfSpaceAfterStar = 3;
         int numberOfStarAfterSpace = 1;
 
         for (int i = 1; i <= n; i++) {
@@ -14,12 +14,22 @@ public abstract class Pattern23 {
                 System.out.print(" ");
             }
             for (int j = 1; j <= numberOfStars; j++) {
-                System.out.print("*");
+                System.out.print("* ");
                 for (int j2 = 1; j2 <= numberOfSpaceAfterStar; j2++) {
                     System.out.print(" ");
                 }
             }
+            if (i < n/2+1){
+                numberOfStars+=2;
+                numberOfSpace--;
+                numberOfSpaceAfterStar-=2;
+            } else {
+                numberOfStars--;
+                numberOfSpace--;
+                numberOfSpaceAfterStar+=3;
+            }
             System.out.println();
+
         }
     }
 }
